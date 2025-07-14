7b4c7d1cc309fef95495da87c588bba81a7b303a
HenriqueMartin5 <henriqueamv86@hotmail.com>
2025-07-14 19:10:23 -0300

    configuracao do arquivo main

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Router from "./routes/routes";
 import GlobalStyles from "./styles/globalStyles";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Router />
-    </BrowserRouter>
+    </HashRouter>
     <GlobalStyles />
   </StrictMode>
 );
